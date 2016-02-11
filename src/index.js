84bacb8c4af266018f6202fb69c78f5c46082bf3
Peter <linusweipert@gmail.com>
2016-02-11 19:01:48 +0100

    bot instantiation

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import * as Bot from "node-telegram-bot-api"
 
 const bot = new Bot("198751085:AAE4MuyxQ3PcBiIF6sAu68EQTl6H08r6wDc", {polling:true})
 
-bot.onText()
+bot.onText(/\/(.+)/, function (msg, match) {
+  if(match[1] === )
+})
