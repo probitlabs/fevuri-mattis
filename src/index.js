33af5480af3884e36126da1aa1379fcc7273b7e5
Te Standby <testandby@gmail.com>
2016-02-14 20:00:27 +0000

    First testing bot

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import * as Bot from "node-telegram-bot-api"
 import * as fs from "fs"
+import * as ht from "http"
+
 const bot = new Bot("198751085:AAE4MuyxQ3PcBiIF6sAu68EQTl6H08r6wDc", {polling:true})
 
-fs.readFile("cert.pem", (err, data)=> bot.setWebhook("https://www.example.com/token", data.toString()))
+fs.readFile("cert.pem", (err, data)=> bot.setWebhook("https://localhost:8009/", data.toString()))
+
+ht.createServer((req)=> console.log(req)).listen(8009);
 
 bot.onText(/\/(.+)/, function (msg, match) {
-  if (match[1] === )
 })
