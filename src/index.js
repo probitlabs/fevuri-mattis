b738bc2e351e4c855721ddb3d94911a3ba45fafe
Te Standby <testandby@gmail.com>
2016-02-14 20:42:49 +0000

    Testing bot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import * as Bot from "node-telegram-bot-api"
+const Bot = require("node-telegram-bot-api") // TODO hack
 import * as fs from "fs"
 import * as ht from "http"
 
-const bot = new Bot("198751085:AAE4MuyxQ3PcBiIF6sAu68EQTl6H08r6wDc", {polling:true})
+console.log(Bot)
+const bot = new Bot("198751085:AAE4MuyxQ3PcBiIF6sAu68EQTl6H08r6wDc", {polling:true, webHook:true})
 
-fs.readFile("cert.pem", (err, data)=> bot.setWebhook("https://localhost:8009/", data.toString()))
+fs.readFile("crt.pem", (err, data)=> {
+  if (err) throw err
+  bot.setWebHook("https://localhost:8009/", data.toString())
+})
+
 
 ht.createServer((req)=> console.log(req)).listen(8009);
 
-bot.onText(/\/(.+)/, function (msg, match) {
-})
+/*bot.onText(/\/(.+)/, function (msg, match) {
+})*/
