3286356575b9ce42bb5250de8613c5ebc0ceccd7
Peter <linusweipert@gmail.com>
2016-02-14 19:47:38 +0100

    Webhook+certifications

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import * as Bot from "node-telegram-bot-api"
-
+import * as fs from "fs"
 const bot = new Bot("198751085:AAE4MuyxQ3PcBiIF6sAu68EQTl6H08r6wDc", {polling:true})
 
+fs.readFile("cert.pem", (err, data)=> bot.setWebhook("https://www.example.com/token", data.toString()))
+
 bot.onText(/\/(.+)/, function (msg, match) {
-  if(match[1] === )
+  if (match[1] === )
 })
