07394267a4b0f2f81c9c463f2388696403e67144
Te Standby <testandby@gmail.com>
2016-03-03 21:06:04 +0000

    Adding https (bug report)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import Bot from "node-telegram-bot-api"
 import * as fs from "fs"
-import * as ht from "http"
+import * as https from "https"
 
 console.log(Bot)
-const bot = new Bot("198751085:AAE4MuyxQ3PcBiIF6sAu68EQTl6H08r6wDc", {polling:true, webHook:true})
+  const bot = new Bot("198751085:AAE4MuyxQ3PcBiIF6sAu68EQTl6H08r6wDc", {webHook:true})
 
 fs.readFile("crt.pem", (err, data)=> {
   if (err) throw err
-  bot.setWebHook("https://localhost:8009/", data.toString())
+  //bot.setWebHook("https://fevuri.us.to:8443/", data.toString())
+  bot.setWebHook("https://80.110.108.196:8443/", data.toString())
 })
 
-
-ht.createServer((req)=> console.log(req)).listen(8009);
+//TODO BUG Make https server
+ht.createServer((req)=> console.log('==NEWS: ', req)).listen(8009);
 
 /*bot.onText(/\/(.+)/, function (msg, match) {
 })*/
